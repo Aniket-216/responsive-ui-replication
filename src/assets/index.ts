eb791eb0322abb9804ed8ba55c15c92788c96ffd
aniket-216 <aniketshinde216@gmail.com>
2024-07-28 21:43:20 +0530

    feat: add photos to the assets

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import UserOne from "./images/User_One.webp";
+import UserTwo from "./images//User_Two.webp";
+import UserThree from "./images//User_Three.webp";
+import UserFour from "./images//User_Four.webp";
+
+export { UserOne, UserTwo, UserThree, UserFour };
